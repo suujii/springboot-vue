11b14dfc06cbf8286f9343dbf45822b9a37df5a6
suji-lee1110 <suji.lee1110@navercorp.com>
2018-05-31 22:05:38 +0900

    [SpringBootVue] front end 추가연동

---
+++ front/src/components/Sample.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="#app">
+    {{sample.id}}
+    {{sample.val}}
+  </div>
+</template>
+
+<script>
+  import {AXIOS} from './http-common'
+
+  export default {
+    el : "#app",
+    data : {
+      response : [],
+      sample : {
+        id : '',
+        val : ''
+      },
+    }
+    methods: {
+      getData: function(){
+        axios.get('/user/suji')
+        .then(function(response){
+          this.response = response.data
+          this.sample.id = response.data.id
+          this.sample.val = response.data.val
+        });
+      }
+    }<template>
+  <div id="#app">
+    {{sample.id}}
+    {{sample.val}}
+  </div>
+</template>
+
+<script>
+  import {AXIOS} from './http-common'
+
+  export default {
+    el : "#app",
+    data : {
+      response : [],
+      errors : [],
+      sample : {
+        id : '',
+        val : ''
+      },
+    }
+    methods: {
+      getData: function(){
+        axios.get('/user/suji')
+        .then(function(response){
+          this.response = response.data
+          this.sample.id = response.data.id
+          this.sample.val = response.data.val
+        }).catch(e => {
+          this.errors.push(e)
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-style: italic
+}
+</style>
+    
+  }
+</script>
+
+<style>
+#app {
+  font-style: italic;
+}
+</style>
